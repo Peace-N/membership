5a7edcd4cce1449f07f1a7e0f635122be8fd3af6
Peace-N <peace@hbros.co.za>
2024-12-15 15:24:50 +0200

    updates

--- resources/views/page/category.blade.php
+++ resources/views/page/category.blade.php
@@ -5,51 +5,50 @@
         </x-heading.h1>
         <p class="pt-4">
             {{ __('This page contains links to policies, agreements for the products and services. To view any of the documents presented on this page, click on the policy/agreement.
-
 ') }}
         </p>
 
     </div>
 
-{{--    <x-blog.post-cards :posts="$posts" />--}}
 
-        <div class="mx-auto w-10/12 mx-auto px-20 p-4 py-8 lg:py-8">
-            <div class="md:flex md:justify-between">
-                <div class="mb-6 md:mb-0">
-                    <x-heading.h3 class="font-semibold">
-                        Wurk Product Agreements and Policies
-                    </x-heading.h3>
-                    <ul class="flex flex-col text-sm mt-2 mb-2">
-                        @foreach($posts as $pages)
-                            <li class="text-sm mb-1 text-gray-300"><a href="{{route('page.view', $pages->slug)}}">{{ $pages->title }}</a></li>
-                        @endforeach
+    <div class="mx-auto w-10/12 mx-auto px-1 lg:px-20 p-4 py-8 lg:py-8">
+        <div class="md:flex md:justify-between">
+            <div class="mb-6 md:mb-0">
+                <x-heading.h3 class="font-semibold">
+                    Wurk Product Agreements and Policies
+                </x-heading.h3>
+                <ul class="flex flex-col text-sm mt-2 mb-2">
+                    @foreach($posts as $pages)
+                        <li class="text-sm mb-1 text-gray-300"><a
+                                href="{{route('page.view', $pages->slug)}}">{{ $pages->title }}</a></li>
+                    @endforeach
 
-                    </ul>
-                </div>
-
-                <div class="mb-6 md:mb-0">
-                    <x-heading.h3 class="font-semibold">
-                        Corporate Information
-                    </x-heading.h3>
-                    <ul class="flex flex-col text-sm mt-2 mb-2">
-                            <li class="text-sm mb-1 text-gray-300"><a href="#">Address:</a></li>
-                            <li class="text-sm mb-1 text-gray-300"><a href="#">Telephone:</a></li>
-                            <li class="text-sm mb-1 text-gray-300"><a href="#">Support:</a></li>
-                    </ul>
-                </div>
-
-                <div class="mb-6 md:mb-0">
-                    <x-heading.h3 class="font-semibold">
-                        Help & Support
-                    </x-heading.h3>
-                    <ul class="flex flex-col text-sm mt-2 mb-2">
-                        <li class="text-sm mb-1 text-gray-300"><a href="#">Global Support Line:</a></li>
-                        <li class="text-sm mb-1 text-gray-300"><a href="#">Chat:</a></li>
-                        <li class="text-sm mb-1 text-gray-300"><a href="#">Discuss:</a></li>
-                    </ul>
-                </div>
+                </ul>
             </div>
 
+            <div class="mb-6 md:mb-0">
+                <x-heading.h3 class="font-semibold">
+                    Corporate Information
+                </x-heading.h3>
+                <ul class="flex flex-col text-sm mt-2 mb-2">
+                    <li class="text-sm mb-1 text-gray-300"><a href="#">Address:</a></li>
+                    <li class="text-sm mb-1 text-gray-300"><a href="#">Telephone:</a></li>
+                    <li class="text-sm mb-1 text-gray-300"><a href="#">Support:</a></li>
+                </ul>
+            </div>
+
+            <div class="mb-6 md:mb-0">
+                <x-heading.h3 class="font-semibold">
+                    Help & Support
+                </x-heading.h3>
+                <ul class="flex flex-col text-sm mt-2 mb-2">
+                    <li class="text-sm mb-1 text-gray-300"><a href="#">Global Support Line:</a></li>
+                    <li class="text-sm mb-1 text-gray-300"><a href="#">Chat:</a></li>
+                    <li class="text-sm mb-1 text-gray-300"><a href="#">Discuss:</a></li>
+                </ul>
+            </div>
         </div>
+
+    </div>
 
 </x-layouts.app>
--- resources/views/page/view.blade.php
+++ resources/views/page/view.blade.php
@@ -2,37 +2,19 @@
 
     <div class="text-center pt-4 pb-0 md:pt-16 md:mb-10">
         <x-heading.h1 class="font-semibold">
-             {{ $post->title }}
+            {{ $post->title }}
         </x-heading.h1>
         <p class="pt-4">
             {{ $post->description }}
         </p>
-
     </div>
+    <div class="mx-auto w-10/12 text-2 px-1 lg:px-20 p-4 py-8 lg:py-8">
 
-        <div class="mx-auto w-10/12 mx-auto px-20 p-4 py-8 lg:py-8">
-            <div class="md:flex md:justify-between">
-                <div class="mb-6 md:mb-0">
-                    <x-heading.h6 class="font-normal">
-                        {!! $post->body !!}
-                    </x-heading.h6>
-
-                </div>
+        <div class="md:flex md:justify-between">
+            <div class="mb-6 md:mb-0">
+                {!! $post->body !!}
             </div>
-
         </div>
 
-{{--    @if (count($morePosts) > 0)--}}
-
-{{--        <div class="text-center">--}}
-{{--            <x-heading.h6 class="text-primary-500">--}}
-{{--                {{ __('Don\'t miss this') }}--}}
-{{--            </x-heading.h6>--}}
-{{--            <x-heading.h2>--}}
-{{--                {{ __('You might also like') }}--}}
-{{--            </x-heading.h2>--}}
-{{--        </div>--}}
-
-{{--    @endif--}}
-
+    </div>
 </x-layouts.app>
